86856c1452c7d2a3ddef8a711f436c9cc1f5e7a4
Potana kundana sai priya <71485076+PotanaKundanaSaiPriya@users.noreply.github.com>
2021-11-15 18:13:38 +0530

    Add files via upload

--- posneg/posneg.sh
+++ posneg/posneg.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "enter a number: "
 read num
-if [ $num -lt 0 ]
-then
-        echo "negative number"
+if [$num -lt 0 ]
+then 
+      echo "negative number"
 elif [ $num -eq 0 ]
 then
-        echo "zero"
+      echo "zero"
 else
-        echo "positive number"
+      echo "positive number"
 fi
