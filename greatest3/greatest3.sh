418430b7e35f4cdb80ef93a8cf32335d05f7580d
Potana kundana sai priya <71485076+PotanaKundanaSaiPriya@users.noreply.github.com>
2021-11-15 18:39:16 +0530

    lab-4

--- greatest3/greatest3.sh
+++ greatest3/greatest3.sh
@@ -1,1 +1,16 @@
-
+#!/bin/sh
+echo "enter first number: "
+read num1
+echo "enter second number: "
+read num2
+echo "enter third number: "
+read num3
+if [ $num1 -gt $num2 -a $num1 -gt $num3 ]
+then
+        echo "$num1 is the largest number"
+elif [ $num2 -gt $num1 -a $num2 -gt $num3 ]
+then
+        echo "$num2 is the largest number"
+else
+        echo "$num3 is the largest number"
+fi
