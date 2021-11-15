db1d19ba4784e798440a9a683375e14fb5ac8954
Potana kundana sai priya <71485076+PotanaKundanaSaiPriya@users.noreply.github.com>
2021-11-15 18:10:10 +0530

    Create posneg.sh

---
+++ posneg/posneg.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "enter a number: "
+read num
+if [ $num -lt 0 ]
+then
+        echo "negative number"
+elif [ $num -eq 0 ]
+then
+        echo "zero"
+else
+        echo "positive number"
+fi
